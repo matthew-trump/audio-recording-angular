96c48f2a27abb0240117470b6da93c4c13322603
matthew-trump <matthew.trump@gmail.com>
2019-04-15 16:03:37 -0700

    component for record rtc recording

---
+++ src/app/record-rtc/record-rtc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-record-rtc',
+  templateUrl: './record-rtc.component.html',
+  styleUrls: ['./record-rtc.component.scss']
+})
+export class RecordRtcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
