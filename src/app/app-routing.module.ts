b704ebff3840fbe5c3d8135407c9c4e38bcf37e6
matthew-trump <matthew.trump@gmail.com>
2019-04-15 15:06:24 -0700

    adding new routes for hark and playback

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SimpleRecordingComponent } from './simple-recording/simple-recording.component';
+import { SimplePlaybackComponent } from './simple-playback/simple-playback.component';
+import { SimplePlaybackHarkComponent } from './simple-playback-hark/simple-playback-hark.component';
 
 const routes: Routes = [
   { path: 'simple', pathMatch: 'full', component: SimpleRecordingComponent },
+  { path: 'playback', pathMatch: 'full', component: SimplePlaybackComponent },
+  { path: 'hark', pathMatch: 'full', component: SimplePlaybackHarkComponent },
 
 ];
 
