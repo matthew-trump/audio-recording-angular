c944a466d72546cd79525f400662427bd2bed46b
matthew-trump <matthew.trump@gmail.com>
2019-04-16 10:14:02 -0700

    route for rtc upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { SimpleRecordingComponent } from './simple-recording/simple-recording.component';
 import { SimplePlaybackComponent } from './simple-playback/simple-playback.component';
 import { SimplePlaybackHarkComponent } from './simple-playback-hark/simple-playback-hark.component';
+import { RecordRtcComponent } from './record-rtc/record-rtc.component';
 
 const routes: Routes = [
   { path: 'simple', pathMatch: 'full', component: SimpleRecordingComponent },
   { path: 'playback', pathMatch: 'full', component: SimplePlaybackComponent },
   { path: 'hark', pathMatch: 'full', component: SimplePlaybackHarkComponent },
+  { path: 'record-rtc', pathMatch: 'full', component: RecordRtcComponent }
 
 ];
 
